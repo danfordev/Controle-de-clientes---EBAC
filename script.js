67d7c857d6478be12d8629fc482ee3954afae92f
Danilo <daniloviana077@hotmail.com>
2026-02-19 11:22:40 -0300

    Controle de clientes -- EBAC

---
+++ script.js
@@ -0,0 +1,44 @@
+const clientes = document.getElementById("controleClientes");
+fetch("https://crudcrud.com/api/6150dc511c644513a088048a20f33d73")
+.then(resposta => resposta.json())
+.then((controleDeClientes) => (
+
+    controleDeClientes.forEach(cliente => {
+        const item = document.createElement("li");
+        item.innerHTML = `${cliente.descricao} <button onclick="remove(${cliente._id})">x</button>`;
+        clientes.appendChild(item);
+        
+    });
+));
+
+document.getElementById("add").addEventListener("click", () => {
+    const descricao = document.getElementById("cliente").value;
+
+    fetch("https://crudcrud.com/api/6150dc511c644513a088048a20f33d73",{
+
+        method: "POST",
+        headers: {
+            "content-type": "applicationa/json"
+        },
+        body: JSON.stringify({descricao: descricao})
+
+    })
+    .then(resposta => resposta.json())
+    .then((cliente) => {
+
+        const item = document.createElement("li");
+        item.innerHTML = `${cliente.descricao} <button onclick="remove(${cliente._id})">x</button>`;
+        clientes.appendChild(item);
+    })
+}
+)
+
+function remove (_id) {
+    fetch("https://crudcrud.com/api/6150dc511c644513a088048a20f33d73",{
+        method: "DELETE",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({_id: _id})
+    });
+}
