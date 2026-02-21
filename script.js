4ca12dbddc1b3c7f6eedf4b01888366f4d371f09
Danilo <daniloviana077@hotmail.com>
2026-02-21 11:42:53 -0300

    correção atividade

--- script.js
+++ script.js
@@ -12,22 +12,26 @@ fetch("https://crudcrud.com/api/6150dc511c644513a088048a20f33d73")
 ));
 
 document.getElementById("add").addEventListener("click", () => {
-    const descricao = document.getElementById("cliente").value;
+    const descricao = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+
+    const cliente = {descricao: descricao, email: email};
+
 
     fetch("https://crudcrud.com/api/6150dc511c644513a088048a20f33d73",{
 
         method: "POST",
         headers: {
-            "content-type": "applicationa/json"
+            "content-type": "application/json"
         },
-        body: JSON.stringify({descricao: descricao})
+        body: JSON.stringify({descricao: descricao, email: email})
 
     })
     .then(resposta => resposta.json())
     .then((cliente) => {
 
         const item = document.createElement("li");
-        item.innerHTML = `${cliente.descricao} <button onclick="remove(${cliente._id})">x</button>`;
+        item.innerHTML = `${cliente.descricao} <button onclick= remove('${cliente._id}')>x</button>`;
         clientes.appendChild(item);
     })
 }
@@ -39,6 +43,5 @@ function remove (_id) {
         headers: {
             "content-type": "application/json"
         },
-        body: JSON.stringify({_id: _id})
     });
 }
